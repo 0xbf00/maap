4e343aad3a01eee097f56756ab7acc74680ba459
Jakob Rieck <jakobrieck@gmail.com>
2018-01-10 16:11:15 +0100

    First versions of dependency and manifest extractors.

---
+++ extractors/dependencies.py
@@ -0,0 +1,114 @@
+from extractors.base import AbstractExtractor, ResultCount
+from bundle.bundle import Bundle
+from bundle.application import Application
+
+import binary.common
+
+import os.path
+import lief
+import json
+
+
+class DependenciesExtractor(AbstractExtractor):
+    """Extracts the direct dependencies of an application
+
+    Dependencies / Loaded libraries and frameworks run inside the same
+    sandbox as the main executable. Therefore, permissions / entitlements
+    are irrelevant. This information is stored, mainly to be able to check
+    for outdated libraries.
+
+    The information is stored as JSON in the file dependencies.json. Each
+    entry contains the dependency's filename, the corresponding bundle-id,
+    as well its version number. If the corresponding bundle could not be found,
+    attempts are made to extract the Info.plist from the binary itself.
+    If that also fails, the information regarding this dependency is thrown out
+    (Because single dylibs contain little information of interest, and these are the
+    only files that should fail)
+    """
+    @classmethod
+    def resource_type(cls):
+        return "dependencies"
+
+    @classmethod
+    def result_count(cls):
+        # The result is just a single JSON file listing the dependencies
+        return ResultCount.SINGLE
+
+    def _info_extract_required(self, info):
+        """Extracts the required keys (CFBundleIdentifier and CFBundleShortVersionString)
+        from the info.plist supplied as a dictionary to this method.
+
+        Returns None in case the information could not be extracted, otherwise returns
+        a dictionary containing the two aforementioned keys."""
+        assert(isinstance(info, dict))
+
+        if not (
+            "CFBundleIdentifier" in info and
+            "CFBundleShortVersionString" in info
+        ):
+            return None
+
+        return {
+            "CFBundleIdentifier": info["CFBundleIdentifier"],
+            "CFBundleShortVersionString": info["CFBundleShortVersionString"]
+        }
+
+    def extract_data(self, app: Bundle, result_path: str) -> bool:
+        # Metadata is stored in a dictionary, which is later serialised to the disk.
+        dependencies_metadata = dict()
+
+        if not isinstance(app, Application):
+            self.log_error("Supplied app {} is not an application".format(app.filepath))
+            return False
+
+        app_dependencies = app.executable().application_libraries()
+        for dependency in app_dependencies:
+            dependency_bundle = binary.common.bundle_from_binary(dependency)
+
+            if not dependency_bundle:
+                if dependency.endswith(".dylib"):
+                    # Dylib's can contain an Info.plist file embedded in them!
+                    # Try to extract this information as a fallback
+                    embedded_info = binary.common.extract_embedded_info(
+                        lief.parse(dependency)
+                    )
+                    if embedded_info:
+                        required_info = self._info_extract_required(embedded_info)
+                        if not required_info:
+                            self.log_error(
+                                "Embedded Info.plist found for dependency {} of application {}, but required keys were not present".format(
+                                    dependency,
+                                    app.filepath
+                                )
+                            )
+                            # It is considered a violation if the Info.plist was extracted but did not contain the
+                            # required keys.
+                            return False
+                        else:
+                            assert(dependency not in dependencies_metadata)
+                            dependencies_metadata[dependency] = required_info
+                    else:
+                        self.log_info("Found DYLIB dependency {} for application {}, ignored because lacking Info.plist".format(
+                            dependency, app.filepath
+                        ))
+                        continue
+
+                self.log_info("Unable to find bundle for dependency {} of app {}".format(
+                    dependency, app.filepath))
+                return False
+            else:
+                required_info = self._info_extract_required(dependency_bundle.info_dictionary())
+                if not required_info:
+                    self.log_error("Info.plist found for dependency {} of app {} does not contain required keys.".format(
+                        dependency, app.filepath
+                    ))
+                    return False
+
+                assert(dependency not in dependencies_metadata)
+                dependencies_metadata[dependency] = required_info
+
+        # Store the information to the filesystem
+        with open(os.path.join(result_path, "dependencies.json"), "w") as outfile:
+            json.dump(dependencies_metadata, outfile, indent=4)
+
+        return True
---
+++ extractors/manifest.py
@@ -0,0 +1,68 @@
+from extractors.base import AbstractExtractor, ResultCount
+from bundle.bundle import Bundle
+from bundle.application import Application
+
+from misc.hash import sha256_file
+
+import os.path
+import json
+
+
+class ManifestExtractor(AbstractExtractor):
+    """Extracts / Builds a manifest for the supplied application
+
+    The manifest consists of one entry per file. Each entry is a dictionary
+    containing as keys the filename, its SHA256 hash and its size. Information
+    about links is not recorded. This file is meant to supplement other information
+    and give insights into meta-information about applications, answering questions
+    such as What is the average grow in size of applications? What is the relation
+    between size of the executable and size of the overall application bundle?
+
+    The information is encoded as a JSON file with the name manifest.json
+    """
+    @classmethod
+    def resource_type(cls):
+        return "manifest"
+
+    @classmethod
+    def result_count(cls):
+        return ResultCount.SINGLE
+
+    def _remove_prefix(self, input: str, prefix: str) -> str:
+        """Removes a prefix from a string, if the string has such a prefix."""
+        if input.startswith(prefix):
+            return input[len(prefix):]
+        return input
+
+    def _build_manifest(self, app) -> list:
+        hashes = dict()
+
+        # Hash all contents of the app
+        for (dirname, dirs, filenames) in os.walk(app.filepath):
+            for filename in filenames:
+                filepath = os.path.join(dirname, filename)
+                if os.path.isfile(filepath) and not os.path.islink(filepath):
+                    hash = sha256_file(filepath)
+                    assert (hash != None)
+                    assert (filepath not in hashes)
+
+                    hashes[filepath] = hash
+
+        output = []
+        for key in hashes.keys():
+            entry = dict()
+            entry["filepath"] = self._remove_prefix(key, app.filepath)
+            entry["filesize"] = os.path.getsize(key)
+            entry["hash"] = hashes[key]
+            output.append(entry)
+
+        return output
+
+    def extract_data(self, app: Bundle, result_path: str) -> bool:
+        assert(isinstance(app, Application))
+
+        manifest = self._build_manifest(app)
+        with open(os.path.join(result_path, "manifest.json")) as outfile:
+            json.dump(manifest, outfile, indent=4)
+
+        return True
