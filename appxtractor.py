7a2a198f3c0b32c25370bf7a43f63ab51c4a7e5d
Jakob Rieck <jakobrieck@gmail.com>
2018-01-12 21:05:41 +0100

    Appxtractor (v0.9.9) is a full-fledged app-information-extractor.

---
+++ appxtractor.py
@@ -0,0 +1,157 @@
+"""
+appxtractor
+
+(c) Jakob Rieck 2018
+
+Tool to extract useful information from macOS .app bundles
+
+What exactly is extracted is dependent on the extractors
+defined in extractors/
+
+Extractor classes need to implement the interface defined in
+extractors/base.py.
+
+Currently, the following data is saved for each version of each app:
+    - The main executable
+    - The main Info.plist
+    - InternetAccessPolicy.plist, if available (this is a Little Snitch specific file)
+    - a manifest containing information regarding each file that is part of the app bundle
+    - executable and Info.plist for each XPC extension thats part of the bundle
+    - Basic information (bundle id and version) for each dependency, where such information
+      is available.
+
+Results are stored in a simple folder structure at a location of the user's choosing. On every
+run, each application is examined (though already processed versions are not re-processed). There
+is no shared state, apart from the results folder.
+"""
+
+import sys
+import os.path
+
+from bundle.bundle import Bundle
+import extractors.base
+import shutil
+import functools
+import logging
+
+
+# ------------------
+# Utility functions
+def folder_for_app(results_dir : str, app : Bundle) -> str:
+    """Return the output directory for a certain application
+    inside the user-specified results directory.
+
+    Note that this function _does not_ touch the filesystem.
+    Therefore, no directories are created as part of this functionality"""
+    app_bundle_id = app.bundle_identifier()
+    app_version = None
+    if "CFBundleShortVersionString" in app.info_dictionary():
+        app_version = app.info_dictionary()["CFBundleShortVersionString"]
+    elif "CFBundleVersion" in app.info_dictionary():
+        app_version = app.info_dictionary()["CFBundleVersion"]
+
+    if not app_version:
+        return None
+
+    return os.path.join(results_dir, app_bundle_id, app_version)
+
+
+def run_extractor(extractor, app, app_result_path) -> bool:
+    """Run a single extractor.
+
+    If the extractor writes out more than a single result, create a dedicated
+    directory for that directory. Returns the status code of the extractor"""
+
+    output_directory = app_result_path
+
+    # Check whether we need to create another extra directory for the results
+    # and create the directory, if needed (when there might be multiple results)
+    if extractor.result_count() == extractors.base.ResultCount.MULTIPLE:
+        output_directory = os.path.join(app_result_path, extractor.resource_type())
+        os.mkdir(output_directory)
+
+    return extractor.extract_data(app, output_directory)
+
+
+def create_logger():
+    logger = logging.getLogger('appxtractor')
+    logger.setLevel(logging.INFO)
+    # Log events to file
+    # We do not log anything to the console, because there is no
+    # user present during execution who could benefit from
+    # such messages. Logs are purely needed for examining defects
+    # after the fact.
+    fh = logging.FileHandler("logs/appxtractor.log")
+    fh.setLevel(logging.INFO)
+    # Custom formatter for log messages
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
+
+
+def main(args):
+    logger = create_logger()
+    logger.info("appxtractor starting")
+
+    # TODO args parsing
+    args = args
+
+    # For now, assume there are 2 (3) arguments:
+    # The first one is the directory to the installed apps,
+    # the second one is the results directory
+    # the third one (optional) is whether to do a dry run
+    # that is, running without touching the disk permanently
+    apps_path = None
+    results_path = None
+    dry_run = len(args) >= 3
+
+    if len(args) >= 2:
+        apps_path = args[0]
+        results_path = args[1]
+
+    print("Assuming apps @ {} and results @ {}".format(apps_path, results_path))
+
+    # Instantiate the extractors once
+    info_extractors = [cls() for cls in extractors.base.all_extractors()]
+
+    for app_path in os.listdir(apps_path):
+        full_path = os.path.join(apps_path, app_path)
+
+        if app_path.endswith(".app") and Bundle.is_bundle(full_path):
+            app = Bundle.make(full_path)
+            if not app.is_mas_app():
+                continue
+
+            output_folder = folder_for_app(results_path, app)
+            if os.path.exists(output_folder):
+                logger.info(
+                    "Skipping processing of {} @ {}, because the app has already been processed.".format(app, app.filepath)
+                )
+                continue
+
+            # Make basefolder
+            os.makedirs(output_folder)
+
+            try:
+                extraction_status = functools.reduce(
+                    lambda status, extractor: status & run_extractor(extractor, app, output_folder),
+                    info_extractors,
+                    True
+                )
+                if not extraction_status:
+                    logger.info("Processing failed for {} @ {}".format(app, app.filepath))
+            except:
+                logger.error(
+                    "Exception occurred during processing of {} @ {}".format(app, app.filepath)
+                )
+
+            # Remove results again from filesystem
+            if dry_run:
+                shutil.rmtree(output_folder)
+
+    logger.info("appxtractor stopping")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
