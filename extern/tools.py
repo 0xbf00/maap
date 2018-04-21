cfb27cd303fd9b07229b8eef925b02d26d264d60
Jakob Rieck <jakobrieck@gmail.com>
2018-04-21 12:08:05 +0200

    Added functionality to call sbpl utility from python.

--- extern/tools.py
+++ extern/tools.py
@@ -1,4 +1,5 @@
 import os.path
+import subprocess
 
 def tool_named(name):
     """Returns the full filepath to the specified tool.
@@ -7,3 +8,28 @@ def tool_named(name):
     if not os.path.exists(tool_path):
         raise ValueError("Tool does not exist.")
     return tool_path
+
+
+def call_sbpl(container, result_format = 'scheme', patch = False):
+    sbpl_base_dir = os.path.join(os.path.dirname(__file__), "../../sbpl")
+    assert os.path.exists(sbpl_base_dir)
+    sbpl_tool = os.path.join(sbpl_base_dir, "build/sbpl")
+    application_base_profile = os.path.join(sbpl_base_dir, "application.sb")
+    assert os.path.exists(sbpl_tool)
+    assert os.path.exists(application_base_profile)
+
+    if result_format != "scheme" and result_format != "json":
+        raise ValueError("Invalid format specified.")
+
+    try:
+        cmd = [sbpl_tool,
+               "--" + result_format,
+               "--container", container,
+               "--profile", application_base_profile]
+        if patch:
+            cmd.append("--patch")
+
+        result = subprocess.check_output(cmd, stderr=subprocess.DEVNULL, cwd=sbpl_base_dir)
+        return result
+    except subprocess.CalledProcessError:
+        return None
