ff0d92e9ec1606a3bdb004fb997122a10cc98cb7
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2018-11-27 13:31:50 +0100

    Add driver script for analysing all applications
    The script iterates over a given folder and runs the appnalyser script
    for each application found. The results are written into a given output
    folder.
    
    Before running this it is recommended to mount your drive containing the
    applications as read-only and to make an APFS snapshot of your test
    machine with `tmutil snapshot`.

---
+++ appnalyser_driver.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import os
+import logging
+import json
+import subprocess
+import sys
+
+from argparse import ArgumentParser
+from termcolor import colored
+from typing import Iterator
+
+from appxtractor import SignalIntelligence
+from misc.logger import create_logger
+
+
+def iterate_applications(directory: str) -> Iterator[str]:
+    for root, dirs, files in os.walk(directory):
+        if root.endswith('.app'):
+            yield root
+
+        app_candidates = [
+            os.path.abspath(os.path.join(root, d))
+            for d in dirs
+            if d.endswith('.app')
+        ]
+
+        dirs[:] = [
+            d
+            for d in dirs
+            if not d.endswith('.app')
+        ]
+
+        for candidate in app_candidates:
+            yield candidate
+
+
+def appnalyse(application_directory: str, applications_directory: str, root_output_directory: str, logger: logging.Logger) -> None:
+    print("[    ] Analysing {}".format(application_directory), end='')
+
+    output_directory = os.path.join(
+        root_output_directory,
+        os.path.relpath(application_directory, applications_directory),
+    )
+    output_fn = os.path.join(output_directory, 'appnalyse_results.json')
+
+    # Skip application if we already obtained results
+    if os.path.exists(output_fn):
+        logger.warning("App already appnalysed: {}. Skipping.".format(application_directory))
+        print("\r[" + colored("skip", 'yellow'))
+        return
+
+    # Run appnalyser for the given application
+    cmd = [
+        './appnalyser.py',
+        application_directory,
+    ]
+    appnalyser = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    try:
+        appnalyser.wait(60)
+    except subprocess.TimeoutExpired:
+        appnalyser.kill()
+        logger.error("Timed out: {}".format(application_directory))
+        print("\r[" + colored("err", 'red'))
+        return
+
+    stdout, stderr = appnalyser.communicate()
+
+    if appnalyser.returncode != 0:
+        logger.error(
+            "appnalyser returned {} for {}: {}".format(
+                appnalyser.returncode,
+                application_directory,
+                stderr.decode(),
+            )
+        )
+        print("\r[" + colored("err ", 'red'))
+        return
+
+    try:
+        json.loads(stdout.decode())
+    except json.JSONDecodeError:
+        logger.error(
+            "appnalyzer did not produce JSON for {}: {}".format(
+                application_directory,
+                stdout.decode(),
+            )
+        )
+        print("\r[" + colored("err ", 'red'))
+        return
+
+    # Store results
+    os.makedirs(output_directory, exist_ok=True)
+    with open(output_fn, 'wb') as fp:
+        fp.write(stdout)
+
+    print("\r[" + colored(" ok", 'green'))
+    logger.info("Successfully appnalysed {}".format(application_directory))
+
+
+def main() -> None:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        'applications',
+        help="Path to the directory, where applications are installed. This folder will be traversed recursively.",
+    )
+    parser.add_argument(
+        'output',
+        help="Path to where the results should be stored",
+    )
+
+    args = parser.parse_args()
+
+    applications_directory = os.path.expanduser(args.applications)
+    output_directory = os.path.expanduser(args.output)
+
+    if not os.path.exists(applications_directory):
+        print("Directory does not exist: {}".format(applications_directory), file=sys.stderr)
+        exit(1)
+
+    exit_watcher = SignalIntelligence()
+
+    logger = create_logger('appnalyser_driver')
+
+    logger.info("appnalyser_driver starting")
+
+    for application_directory in iterate_applications(applications_directory):
+        if exit_watcher.should_exit:
+            break
+
+        appnalyse(application_directory, applications_directory, output_directory, logger)
+
+    logger.info("appnalyser_driver stopping")
+
+
+if __name__ == '__main__':
+    main()
