307782a0a3d98410acab6fe728ccc6f8de07bed8
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2020-04-24 14:02:53 +0200

    Create abstract driver class
    This class can then be used to implement batch processing of other
    analysers.

--- appnalyser_driver.py
+++ appnalyser_driver.py
@@ -1,111 +1,76 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
-import logging
 import json
 import subprocess
-import sys
 
-from argparse import ArgumentParser
-from termcolor import colored
-from typing import Iterator
-
-from appxtractor import folder_for_app, SignalIntelligence
+import driver
 from bundle.bundle import Bundle
-from misc.logger import create_logger
 
 
-def iterate_applications(directory: str) -> Iterator[str]:
-    for root, dirs, files in os.walk(directory):
-        if root.endswith('.app'):
-            yield root
+class AppnalyseDriver(driver.Driver):
 
-        app_candidates = [
-            os.path.abspath(os.path.join(root, d))
-            for d in dirs
-            if d.endswith('.app')
-        ]
+    def __init__(self) -> None:
+        super().__init__('appnalyser_driver')
 
-        dirs[:] = [
-            d
-            for d in dirs
-            if not d.endswith('.app')
-        ]
+    def analyse(self, app: Bundle, out_dir: str) -> driver.Result:
+        out_fn = os.path.join(out_dir, 'appnalyse.json')
 
-        for candidate in app_candidates:
-            yield candidate
+        # Skip application if we already obtained results
+        if os.path.exists(out_fn):
+            self.logger.warning(f"App already appnalysed: {app.filepath}. Skipping.")
+            return driver.Result.SKIPPED
 
-
-def appnalyse(application_directory: str, root_output_directory: str, logger: logging.Logger) -> None:
-    print("[    ] Analysing {}".format(application_directory))
-    reset_cursor = "\r\033[1A["
-
-    app = Bundle.make(application_directory)
-    output_directory = folder_for_app(root_output_directory, app)
-
-    output_fn = os.path.join(output_directory, 'appnalyse_results.json')
-
-    # Skip application if we already obtained results
-    if os.path.exists(output_fn):
-        logger.warning("App already appnalysed: {}. Skipping.".format(application_directory))
-        print(reset_cursor + colored("skip", 'yellow'))
-        return
-
-    # Run appnalyser for the given application
-    cmd = [
-        './appnalyser.py',
-        application_directory,
-    ]
-    appnalyser = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    try:
-        appnalyser.wait(60)
-    except subprocess.TimeoutExpired:
-        appnalyser.kill()
-        logger.error("Timed out: {}".format(application_directory))
-        print(reset_cursor + colored("err", 'red'))
-        return
-
-    stdout, stderr = appnalyser.communicate()
-
-    if appnalyser.returncode != 0:
-        logger.error(
-            "appnalyser returned {} for {}: {}".format(
-                appnalyser.returncode,
-                application_directory,
-                stderr.decode(),
+        # Run appnalyser for the given application
+        try:
+            subprocess.run
+            appnalyser = subprocess.run(
+                ['./appnalyser.py', app.filepath],
+                capture_output=True,
+                text=True,
+                timeout=60,
             )
-        )
-        print(reset_cursor + colored("err ", 'red'))
-        return
+        except subprocess.TimeoutExpired:
+            self.logger.error(f"Timed out: {app.filepath}")
+            return driver.Result.ERROR
 
-    try:
-        json.loads(stdout.decode())
-    except json.JSONDecodeError:
-        logger.error(
-            "appnalyzer did not produce JSON for {}: {}".format(
-                application_directory,
-                stdout.decode(),
+        if appnalyser.returncode != 0:
+            self.logger.error(
+                "appnalyser returned {} for {}: {}".format(
+                    appnalyser.returncode,
+                    app.filepath,
+                    appnalyser.stderr,
+                )
             )
-        )
-        print(reset_cursor + colored("err ", 'red'))
-        return
+            return driver.Result.ERROR
 
-    # Store results
-    os.makedirs(output_directory, exist_ok=True)
-    with open(output_fn, 'wb') as fp:
-        fp.write(stdout)
+        try:
+            json.loads(appnalyser.stdout)
+        except json.JSONDecodeError:
+            self.logger.error(
+                f"appnalyzer did not produce valid JSON for {app.filepath}: {appnalyser.stdout}"
+            )
+            return driver.Result.ERROR
 
-    print(reset_cursor + colored(" ok", 'green'))
-    logger.info("Successfully appnalysed {}".format(application_directory))
+        # Store results
+        os.makedirs(out_dir, exist_ok=True)
+        with open(out_fn, 'w') as fp:
+            fp.write(appnalyser.stdout)
+
+        self.logger.info(f"Successfully appnalysed {app.filepath}")
+        return driver.Result.OK
 
 
 def main() -> None:
-    parser = ArgumentParser()
+    parser = argparse.ArgumentParser()
 
     parser.add_argument(
         'applications',
-        help="Path to the directory, where applications are installed. This folder will be traversed recursively.",
+        help="""
+            Path to the directory, where applications are installed. This
+            folder will be traversed recursively.
+        """,
     )
     parser.add_argument(
         'output',
@@ -114,26 +79,11 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    applications_directory = os.path.expanduser(args.applications)
-    output_directory = os.path.expanduser(args.output)
+    apps_dir = os.path.expanduser(args.applications)
+    out_dir = os.path.expanduser(args.output)
 
-    if not os.path.exists(applications_directory):
-        print("Directory does not exist: {}".format(applications_directory), file=sys.stderr)
-        exit(1)
-
-    exit_watcher = SignalIntelligence()
-
-    logger = create_logger('appnalyser_driver')
-
-    logger.info("appnalyser_driver starting")
-
-    for application_directory in iterate_applications(applications_directory):
-        if exit_watcher.should_exit:
-            break
-
-        appnalyse(application_directory, output_directory, logger)
-
-    logger.info("appnalyser_driver stopping")
+    driver = AppnalyseDriver()
+    driver.run(apps_dir, out_dir)
 
 
 if __name__ == '__main__':
---
+++ driver.py
@@ -0,0 +1,87 @@
+import abc
+import enum
+import os
+import sys
+
+from termcolor import colored
+from typing import Iterator
+
+from appxtractor import folder_for_app, SignalIntelligence
+from bundle.bundle import Bundle
+from misc.logger import create_logger
+
+
+class Result(str, enum.Enum):
+    OK = " ok "
+    ERROR = "err "
+    SKIPPED = "skip"
+
+    @property
+    def color(self) -> str:
+        if self == Result.OK:
+            return 'green'
+        elif self == Result.ERROR:
+            return 'red'
+        elif self == Result.SKIPPED:
+            return 'yellow'
+        else:
+            assert False, f"Unhandled case: {self}"
+
+    @property
+    def colored(self) -> str:
+        return colored(self.value, self.color)
+
+
+class Driver(abc.ABC):
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.logger = create_logger(name)
+
+    def run(self, apps_dir: str, out_dir: str) -> None:
+        if not os.path.exists(apps_dir):
+            print(f"Directory does not exist: {apps_dir}", file=sys.stderr)
+            exit(1)
+
+        exit_watcher = SignalIntelligence()
+
+        self.logger.info(f"{self.name} starting")
+
+        for app_dir in Driver.iterate_applications(apps_dir):
+            if exit_watcher.should_exit:
+                break
+
+            app = Bundle.make(app_dir)
+            app_out_dir = folder_for_app(out_dir, app)
+
+            print(f"[    ] Analysing {app.filepath}")
+            reset_cursor = "\r\033[1A["
+            result = self.analyse(app, app_out_dir)
+            print(reset_cursor + result.colored)
+
+        self.logger.info(f"{self.name} stopping")
+
+    @abc.abstractmethod
+    def analyse(self, app: Bundle, out_dir: str) -> Result:
+        raise NotImplementedError
+
+    @staticmethod
+    def iterate_applications(directory: str) -> Iterator[str]:
+        for root, dirs, files in os.walk(directory):
+            if root.endswith('.app'):
+                yield root
+
+            app_candidates = [
+                os.path.abspath(os.path.join(root, d))
+                for d in dirs
+                if d.endswith('.app')
+            ]
+
+            dirs[:] = [
+                d
+                for d in dirs
+                if not d.endswith('.app')
+            ]
+
+            for candidate in app_candidates:
+                yield candidate
